bd7b77f4b67f21601371042afd5fcd38bd31f0ae
Dilhan Amarasena <damarasena@virtusa.com>
2021-05-18 14:18:32 +0530

    UI changes

--- src/app/formdata/formdata.component.ts
+++ src/app/formdata/formdata.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { CRUDService } from '../autoclub-data-table/crud.service';
 @Component({
   selector: 'app-formdata',
   templateUrl: './formdata.component.html',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormdataComponent implements OnInit {
 
-  constructor() { }
+  constructor(public crudService: CRUDService,    
+    private apollo: Apollo,  
+    ) { }
 
   ngOnInit(): void {
+    this.fetchData()
   }
 
   name="Dilhan";
@@ -24,4 +29,30 @@ export class FormdataComponent implements OnInit {
   {"name":"dilhan","age":"20","address":"col","school":"jvc","country":"Lk"},
   {"name":"dilhan","age":"20","address":"col","school":"jvc","country":"Lk"}
             ]
+
+ 
+
+  fetchData(){ 
+    return this.apollo.watchQuery({query : gql`
+              query{
+                findAllData{
+                id
+                  lastName
+                  firstName
+                  email
+                  carModel
+                  carMake
+                  ageOfVehicle
+                  manufacturedDate
+                }
+              }      
+              `
+
+  }).valueChanges.subscribe(({ data }: any) => {
+    
+    //console.log(data.findAllData);
+      this.dataList =data.findAllData;
+    })      
+
+  } 
 }
